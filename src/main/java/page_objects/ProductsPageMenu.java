52a3024e62bce6f6e6dfa5bb45a827e3c096590e
Paula <PaulaRoxana>
2022-07-09 12:56:42 +0300

    08.07.2022

--- src/main/java/page_objects/ProductsPageMenu.java
+++ src/main/java/page_objects/ProductsPageMenu.java
@@ -6,12 +6,25 @@ import org.openqa.selenium.WebDriver;
 public class ProductsPageMenu {
 
     private WebDriver driver;
-    private By menuButton = By.id("react-burger-menu-btn");
-
+    private By menuButton = By.cssSelector("#react-burger-menu-btn");
+    private By unwrappedMenuButton = By.cssSelector(".bm-item-list");
 
     public ProductsPageMenu(WebDriver driver) {
         this.driver=driver;
     }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public By getMenuButton() {
+        return menuButton;
+    }
+
+    public By getUnwrappedMenuButton() {
+        return unwrappedMenuButton;
+    }
+
 
 
 }
--- src/test/java/stepdefinitions/ProductsPageSteps.java
+++ src/test/java/stepdefinitions/ProductsPageSteps.java
@@ -1,6 +1,18 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import page_objects.Application;
+import page_objects.LoginPage;
+import page_objects.ProductsPageMenu;
+
+import static stepdefinitions.Hooks.driver;
+
 public class ProductsPageSteps {
-
-
+    @And("Click on the menu buton")
+    public void clickOnTheMenuButon() {
+    }
 }
