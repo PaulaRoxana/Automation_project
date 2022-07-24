e7a215df7632bba7d0f5aeebdd32cc247c9d989d
Paula <PaulaRoxana>
2022-07-24 13:27:08 +0300

    24.07.2022

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -13,18 +13,20 @@ import static stepdefinitions.Hooks.driver;
 
 public class LoginSteps {
     LoginPage loginPage = new LoginPage(driver);
-    ProductsPage productsPage=new ProductsPage(driver);
+    ProductsPage productsPage = new ProductsPage(driver);
 
 
     @Given("Open the Saucedemo web page")
     public void openTheSaucedemoWebPage() {
         loginPage = new Application(driver).navigateToLoginPage();
     }
+
     @When("Fill the Username field with username {string}")
     public void fillTheUsernameFieldWithCorrectUsername(String text) {
         loginPage.fillUsername(text);
 
     }
+
     @And("Fill the Password field with password {string}")
     public void fillThePasswordFieldWithCorrectPassword(String text) {
         loginPage.fillPassword(text);
@@ -35,7 +37,7 @@ public class LoginSteps {
         loginPage.submit();
     }
 
-     @Then("Validate the error message for incorrect Username and or Password is correct")
+    @Then("Validate the error message for incorrect Username and or Password is correct")
     public void validateTheErrorMessageForIncorrectUsernameAndOrPasswordIsCorrect() {
 
         Assert.assertTrue(loginPage.getErrorMessageForIncorrectUsernameOrPassword()
@@ -47,6 +49,7 @@ public class LoginSteps {
 
         Assert.assertTrue(loginPage.getErrorMessageForMissingUsername().contains("Epic sadface: Username is required"));
     }
+
     @Then("Validate the error message for missing Password is correct")
     public void validateTheErrorMessageForMissingPasswordIsCorrect() {
         Assert.assertTrue(loginPage.getErrorMessageForMissingPassword().contains("Epic sadface: Password is required"));
